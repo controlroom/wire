d77af565e187fba7f9ed0f451928cea47e6ff387
rabidpraxis <kevin@kevinwebster.me>
2015-07-22 14:38:40 -0700

    0.1.1-SNAPSHOT, bumped clj to 1.7 and removed cljx

--- project.clj
+++ project.clj
@@ -1,16 +1,7 @@
-(defproject controlroom/wire "0.1.0"
+(defproject controlroom/wire "0.1.1-SNAPSHOT"
+  :jar-exclusions  [#"\.cljx|\.swp|\.swo|\.DS_Store"]
   :description "Sane intercomponent communication"
   :url "http://controlroom.io/wire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-
-  :plugins [[com.keminglabs/cljx "0.6.0"]]
-
-  :cljx {:builds [{:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :clj}
-
-                  {:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :cljs}]})
+  :dependencies [[org.clojure/clojure "1.7.0"]])
