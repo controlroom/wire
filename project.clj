52fef54db24b7165eadbeeab30af86b541264fe6
Kevin Webster <kwebster@renewfinancial.com>
2018-06-13 16:40:59 -0700

    Bumped to 0.2.0

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject controlroom/wire "0.1.1"
+(defproject controlroom/wire "0.2.0"
   :jar-exclusions  [#"\.cljx|\.swp|\.swo|\.DS_Store"]
   :description "Sane intercomponent communication"
   :url "http://controlroom.io/wire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :cljsbuild {:builds []}
-  )
+  :cljsbuild {:builds []})
